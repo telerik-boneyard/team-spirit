8892030509f5e3a2720aefd5702aaba92bd2d5ac
Georgi Prodanov <georgi.prodanov@progress.com>
2017-01-03 12:02:03 +0200

    Exclude js and map files from repo.

--- app/services/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./everlive-provider.service'));
-__export(require('./users.service'));
-//# sourceMappingURL=index.js.map
