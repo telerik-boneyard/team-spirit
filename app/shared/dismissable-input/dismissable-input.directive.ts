a4206aef984eb16b770fce8eb06d70aabe740332
WorkMac <backendservtestuser@backendservmac.bedford.progress.com>
2017-03-10 15:46:42 +0200

    Fix selfclosing keyboard on iOS

--- app/shared/dismissable-input/dismissable-input.directive.ts
+++ app/shared/dismissable-input/dismissable-input.directive.ts
@@ -1,20 +1,35 @@
-import { Directive, OnInit, ElementRef } from '@angular/core';
-import { TextField } from 'ui/text-field';
-import { Page } from 'ui/page';
-import * as viewModule from 'ui/core/view';
+import { Directive, OnInit, Input, ElementRef } from '@angular/core';
+// import { TextField } from 'ui/text-field';
+import { PlatformService } from '../../services';
 
 @Directive({
     selector: '[dismissable]'
 })
 export class DismissableInputDirective implements OnInit {
+    @Input('dismissable') anchorName: string;
     private _field: any;
+    private _wasFieldClick: boolean = false;
     
-    constructor(el: ElementRef) {
+    constructor(el: ElementRef, private _platform: PlatformService) {
         this._field = el.nativeElement;
+        this._field.on('tap', () => this._wasFieldClick = true);
     }
 
     ngOnInit() {
-        let anchor = viewModule.getViewById(this._field.page, 'dismissableAnchor');
-        anchor.on('tap', () => this._field.dismissSoftInput());
+        let anchor = this._field.page.getViewById(this.anchorName || 'dismissableAnchor');
+        anchor.on('tap', (args) => {
+            if (this._platform.isAndroid) {
+                return this._field.dismissSoftInput();
+            }
+
+            // hack to keep keyboard open on iOS cause of
+            // different event propagation to Android
+            setTimeout(() => {
+                if (!this._wasFieldClick) {
+                    this._field.dismissSoftInput();
+                }
+                this._wasFieldClick = false;
+            }, 0);
+        });
     }
 }
