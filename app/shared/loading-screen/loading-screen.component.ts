95c9f20a4781c3a12de9b90ef9c961d4d4116036
Georgi Prodanov <georgi.prodanov@progress.com>
2017-03-10 13:56:02 +0200

    Add loading screen to all events screens.

--- app/shared/loading-screen/loading-screen.component.ts
+++ app/shared/loading-screen/loading-screen.component.ts
@@ -14,37 +14,47 @@ export class LoadingScreenComponent implements OnInit {
     @Input('hideElement') hideElementId: string;
     showLoader: boolean = false;
     private _hideElement: View;
+    private _extendedLoadingSub: any;
+    private _allLoaded: any;
     
     constructor(
         private _loadingService: LoadingIndicatorService,
         private _page: Page
-    ) {
-
-    }
+    ) {}
 
     ngOnInit() {
         this._hideElement = this._page.getViewById(this.hideElementId);
         this._subscribeToLoadingEvents();
+
+        this._page.on('navigatingTo', data => {
+            this._subscribeToLoadingEvents();
+        });
+        this._page.on('navigatingFrom', data => {
+            this._unsubscriebFromLoadingEvents();
+        });
     }
 
     private _subscribeToLoadingEvents() {
-        this._loadingService.extendedLoading.subscribe(() => {
+        this._extendedLoadingSub = this._loadingService.extendedLoading.subscribe(() => {
             this._startLoading();
         });
-        this._loadingService.allLoaded.subscribe(() => {
+        this._allLoaded = this._loadingService.allLoaded.subscribe(() => {
             this._endLoading();
         });
     }
 
+    private _unsubscriebFromLoadingEvents() {
+        this._extendedLoadingSub.unsubscribe();
+        this._allLoaded.unsubscribe();
+    }
+
     private _startLoading() {
-        console.log('start called');
         this._hideElement.visibility = 'collapse';
         this.showLoader = true;
         this._page.actionBarHidden = true;
     }
 
     private _endLoading() {
-        console.log('end called');
         this._hideElement.visibility = 'visible';
         this.showLoader = false;
         this._page.actionBarHidden = false;
